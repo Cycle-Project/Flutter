83ed3e76c790fb3904c914cc88894eed4394c7f9
ozturkomerfaruk <omerfarukozturk026@gmail.com>
2022-11-28 13:08:30 +0300

    fix

--- lib/Page/LandingPage/map/components/plan_route.dart
+++ lib/Page/LandingPage/map/components/plan_route.dart
@@ -33,6 +33,7 @@ class PlanRoute extends HookWidget {
               backgroundColor: Colors.blue,
               shadowColor: Colors.transparent,
               height: 60,
+              borderRadius: !enlarge.value ? BorderRadius.circular(10) : const BorderRadius.vertical(top: Radius.circular(10)),
               child: Row(
                 children: [
                   Padding(
@@ -90,17 +91,8 @@ class PlanRoute extends HookWidget {
             duration: const Duration(milliseconds: 750),
             height: height.value,
             child: Container(
-              decoration: BoxDecoration(
+              decoration: const BoxDecoration(
                 color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: const [
-                  BoxShadow(
-                    color: Colors.grey,
-                    blurRadius: 1,
-                    spreadRadius: 0,
-                    offset: Offset(2.5, 3),
-                  )
-                ],
               ),
             ),
           )
