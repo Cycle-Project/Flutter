217f10ff0e0650acf57bfc7103f38e0a88276ffc
ozturkomerfaruk <omerfarukozturk026@gmail.com>
2023-01-13 10:15:21 +0300

    Unit Test

---
+++ test/route_unit_test.dart
@@ -0,0 +1,84 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:geo_app/Client/Controller/route_controller.dart';
+import 'package:geo_app/Client/Controller/user_controller.dart';
+import 'package:geo_app/Client/Models/Route/route.dart';
+import 'package:geo_app/main.dart';
+
+void main() {
+  group("Route Unit Test", () {
+    late UserController userController;
+    late RouteController routeController;
+
+    setUp(() {
+      routeController = RouteController();
+      userController = UserController();
+    });
+
+    test("create-route", () async {
+      await userController
+          .login({"email": "yahya@gmail.com", "password": "qwe123"});
+
+      await routeController.createRoute({
+        "positions": [
+          {
+            "latitude": "123123",
+            "longitude": "123123",
+            "altitude": "123123",
+            "city": "TestDeneme"
+          }
+        ],
+        "userMadeId": applicationUserModel.id
+      }, token: applicationUserModel.token!);
+
+      List<Route> routeList =
+          await routeController.getRoutes(token: applicationUserModel.token!);
+
+      expectLater(routeList.last.positions!.last.latitude, "123123");
+      expectLater(routeList.last.positions!.last.city, "TestDeneme");
+      expectLater(routeList.last.positions!.last.longitude, "123123");
+      expectLater(routeList.last.positions!.last.altitude, "123123");
+    });
+
+    //TODO: SADECE ID GÖNDERİYOR - POSTMANDE BİR SIKINTI YOK BABA
+    test("add-position", () async {
+      await userController
+          .login({"email": "yahya@gmail.com", "password": "qwe123"});
+
+      await routeController.addPosition(
+        {
+          "positions": [
+            {
+              "latitude": "111",
+              "longitude": "222",
+              "altitude": "121212",
+              "city": "omerfaruk",
+            }
+          ],
+          "userMadeId": applicationUserModel.id
+        },
+        token: applicationUserModel.token!,
+        id: "63c0fea2f27855cc5614b686",
+      );
+
+      List<Route> routeList =
+          await routeController.getRoutes(token: applicationUserModel.token!);
+
+     routeList.forEach((i) {
+       if (i.id == "63c0fea2f27855cc5614b686") {
+         print(i.positions!.last.city);
+       }
+     });
+
+    });
+
+    test("route-list", () async {
+      await userController
+          .login({"email": "yahya@gmail.com", "password": "qwe123"});
+
+      List<Route> routeList =
+          await routeController.getRoutes(token: applicationUserModel.token!);
+
+      expectLater(routeList.isEmpty, false);
+    });
+  });
+}
