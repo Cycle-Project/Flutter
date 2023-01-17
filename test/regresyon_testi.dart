8550b31f196bb2f2c74c61d2da5bcc35c2069abb
ozturkomerfaruk <omerfarukozturk026@gmail.com>
2023-01-17 20:04:44 +0300

    fix

--- test/regresyon_testi.dart
+++ test/regresyon_testi.dart
@@ -3,17 +3,6 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('app load test', () async {
-    // Arrange
-    final testWidgetsFlutterBinding =
-        TestWidgetsFlutterBinding.ensureInitialized();
-    await testWidgetsFlutterBinding.runAsync(() async {
-      // Act
-      await TestApp.start();
-      final loginButtonFinder = find.byKey(Key('loginButton'));
-      await tester.pump();
 
-      // Assert
-      expect(loginButtonFinder, findsOneWidget);
     });
-  });
 }
